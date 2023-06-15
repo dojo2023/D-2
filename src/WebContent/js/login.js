b1eb4a8a1c3de018ca093995d69e9036689b3f29
dojo2023-mukai <130334925+dojo2023-mukai@users.noreply.github.com>
2023-06-15 11:16:11 +0900

    Update login.js

--- src/WebContent/js/login.js
+++ src/WebContent/js/login.js
@@ -1,3 +1,15 @@
-/**
- *
- */
+function check() {
+	document.getElementById('id').style.backgroundColor = '#ffffff';
+	document.getElementById('pw').style.backgroundColor = '#ffffff';
+	if (auth.id.value == '' || auth.pw.value == '') {
+		if (auth.id.value == '') {
+			document.getElementById('id').style.backgroundColor = '#eebbcb';
+		}
+		if(auth.pw.value == '') {
+			document.getElementById('pw').style.backgroundColor = '#eebbcb';
+		}
+		return false;
+	} else {
+		return true;
+	}
+}
