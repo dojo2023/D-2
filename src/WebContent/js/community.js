0d068310f29bcc53ae78f9b0e0eb4941a27bc697
dojo2023-nakashima <130334544+dojo2023-nakashima@users.noreply.github.com>
2023-06-20 15:56:09 +0900

    訂正

--- src/WebContent/js/community.js
+++ src/WebContent/js/community.js
@@ -14,3 +14,13 @@ $(function() {
         messageSubmit.disabled = false;
       }
 })
+
+ const applyTextLimit = () => {
+  let maxLength = 55; //上限文字数
+  let limitedText = document.getElementById('limited-text');
+  let originalText = limitedText.innerText;
+  if (originalText.length > maxLength) {
+    limitedText.innerText = originalText.substr(0, maxLength) + '...';
+  }
+}
+applyTextLimit();
