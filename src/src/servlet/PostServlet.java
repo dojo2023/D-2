d5876699d83f8bc8ae6b9762a2afa8d8380d0524
dojo2023-fujita <130331187+dojo2023-fujita@users.noreply.github.com>
2023-06-22 17:16:33 +0900

    Update PostServlet.java

--- src/src/servlet/PostServlet.java
+++ src/src/servlet/PostServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,14 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
-import dao.ArticleDao;
-import model.Article;
-
 /**
  * Servlet implementation class PostServlet
  */
 @WebServlet("/PostServlet")
-@MultipartConfig
+@MultipartConfig (location = "C:\\dojo6\\product_D2\\WebContent\\upload",maxFileSize=700000) // アップロードファイルの一時的な保存先
 public class PostServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -41,37 +37,21 @@ public class PostServlet extends HttpServlet {
 		//リクエストパラメータを取得する。（Article型の変数に格納）
 		HttpSession session = request.getSession();
 		request.setCharacterEncoding("UTF-8");
-		String img1Path="", img2Path="", img3Path="";
-
-		//仮に記事を作成
-				Article tentative = new Article();
-				String tentativeLang[] = new String[16];
-				String tentativePurp[] = new String[11];
-				String tentativeCert[] = new String[14];
-				Arrays.fill(tentativeLang,"");
-				Arrays.fill(tentativePurp, "");
-				Arrays.fill(tentativeCert,"");
-
-				tentative.setArticleLanguage(tentativeLang);
-				tentative.setArticlePurpose(tentativePurp);
-				tentative.setArticleCertification(tentativeCert);
-
-				ArticleDao aDao = new ArticleDao();
-				aDao.insert(tentative);
+		Part articleImg1 = null;
+		Part articleImg2 = null;
+		Part articleImg3 = null;
+		String errorMessage = "";
+		String articleImg1Path = "",articleImg2Path = "",articleImg3Path = "";
 
 
 		//各値を取得
-				String articleTitle = request.getParameter("title");
-				String userId = (String)session.getAttribute("user");
-				String[] articleLanguage = request.getParameterValues("language");
-				String[] articlePurpose = request.getParameterValues("purpose");
-				String articleCareer = request.getParameter("career");
-				String[] articleCertification = request.getParameterValues("certification");
-				String articleText = request.getParameter("text");
-				//name属性ごとにファイルをPartオブジェクトとして取得
-				Part articleImg1 = request.getPart("img1");
-				Part articleImg2 = request.getPart("img2");
-				Part articleImg3 = request.getPart("img3");
+		String articleTitle = request.getParameter("title");
+		String userId = (String)session.getAttribute("user");
+		String[] articleLanguage = request.getParameterValues("language");
+		String[] articlePurpose = request.getParameterValues("purpose");
+		String articleCareer = request.getParameter("career");
+		String[] articleCertification = request.getParameterValues("certification");
+		String articleText = request.getParameter("text");
 
 		//リクエストスコープに格納
 		request.setAttribute("articleTitle",articleTitle);
@@ -81,35 +61,89 @@ public class PostServlet extends HttpServlet {
 		request.setAttribute("articleCareer",articleCareer);
 		request.setAttribute("articleCertification",articleCertification);
 		request.setAttribute("articleText",articleText);
-		request.setAttribute("articleImg1",articleImg1);
-		request.setAttribute("articleImg2",articleImg2);
-		request.setAttribute("articleImg3",articleImg3);
+		session.setAttribute("articleImg1",articleImg1);
+		session.setAttribute("articleImg2",articleImg2);
+		session.setAttribute("articleImg3",articleImg3);
 
+		try {
+			//name属性ごとにファイルをPartオブジェクトとして取得
+			articleImg1 = request.getPart("img1");
+			if(articleImg1.getSize()!=0) {
+				String ImgName1 = "tempImg1";
+				String extName = this.getFileName(articleImg1);
+				int idx = extName.lastIndexOf(".");
+				extName = extName.substring(idx);
+				ImgName1 += extName;
+				articleImg1.write(ImgName1);
+				//System.out.printf("%s\n",ImgName1 );	//確認用
+				articleImg1Path = "/product_D2/upload/"+ImgName1;
+			}else if (articleImg1.getSize()==0) {
+					articleImg1Path = "";
+				}
+		}
+		catch(IllegalStateException e) {
+			articleImg1Path = "";
+			errorMessage = "ファイルのサイズが大きすぎるのでアップロードできませんでした。";
+		}
+
+		try {
+			articleImg2 = request.getPart("img2");
+			if(articleImg2.getSize()!=0) {
+				String ImgName2 = "tempImg2";
+				String extName = this.getFileName(articleImg2);
+				int idx = extName.lastIndexOf(".");
+				extName = extName.substring(idx);
+				ImgName2 += extName;
+				articleImg1.write(ImgName2);
+				//System.out.printf("%s\n",ImgName1 );	//確認用
+				articleImg2Path = "/product_D2/upload/"+ImgName2;
+			} else if (articleImg2.getSize()==0) {
+				articleImg2Path = "";
+			}
+		}
+		catch(IllegalStateException e) {
+			articleImg2Path = "";
+			errorMessage = "ファイルのサイズが大きすぎるのでアップロードできませんでした。";
+		}
+
+		try {
+			articleImg3 = request.getPart("img3");
+			if(articleImg3.getSize()!=0) {
+				String ImgName3 = "tempImg3";
+				String extName = this.getFileName(articleImg3);
+				int idx = extName.lastIndexOf(".");
+				extName = extName.substring(idx);
+				ImgName3 += extName;
+				articleImg1.write(ImgName3);
+				//System.out.printf("%s\n",ImgName1 );	//確認用
+				articleImg3Path = "/product_D2/upload/"+ImgName3;
+			}else if (articleImg3.getSize()==0) {
+				articleImg3Path = "";
+				}
+		}
+		catch(IllegalStateException e) {
+			articleImg3Path = "";
+			errorMessage = "ファイルのサイズが大きすぎるのでアップロードできませんでした。";
+		}
+
+
+
+		request.setAttribute("articleImg1Path",articleImg1Path);
+		request.setAttribute("articleImg2Path",articleImg2Path);
+		request.setAttribute("articleImg3Path",articleImg3Path);
+		request.setAttribute("errorMessage", errorMessage);
+
+
+		try {
+			Thread.sleep(5000);
+		}
+		catch(InterruptedException e){
+			Thread.currentThread().interrupt();
+		}
 
 		//確認ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/PostResult.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/post_confirm.jsp");
 		dispatcher.forward(request, response);
-
-
-
-		/* 以下登録機能はPostConfirmServlet.javaにて行う
-		登録処理を行う
-		ArticleDao aDao = new ArticleDao();
-		if(aDao.insert(new Article(articleId, articleTitle, userId, articleCreate,
-				 articleUpdate, articleLanguage, articlePurpose, articleCareer, articleCertification,
-				 articleFavs, articleText, articleImg1, articleImg2, articleImg3) )) {
-		String message = "登録しました！";
-		request.setAttribute("message", message);
-		}
-		else {
-			String message = "登録に失敗しました";
-			request.setAttribute("message", message);
-		}
-		//結果ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/PostResult.jsp");
-		dispatcher.forward(request, response);
-		*/
-
 	}
 
 	// ファイル名を取得するメソッド
@@ -126,7 +160,9 @@ public class PostServlet extends HttpServlet {
 
 
 
-
 }
+
+
+
 
 
