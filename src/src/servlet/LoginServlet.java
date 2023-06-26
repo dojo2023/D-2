f4ee9b7e268e078369837d8a43f59c5bdec4a4b7
dojo2023-mukai <130334925+dojo2023-mukai@users.noreply.github.com>
2023-06-26 15:24:50 +0900

    Update LoginServlet.java

--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -45,7 +45,7 @@ public class LoginServlet extends HttpServlet {
 		String pw = request.getParameter("pw");
 		User user = new User(id, pw);
 		UserDao uDao = new UserDao();
-		if (uDao.isLoginOK(user)) {
+		if (!uDao.isLoginOK(user).getUserId().equals("")) {
 			HttpSession session = request.getSession();
 			session.setAttribute("user", user);
 			response.sendRedirect("/product_D2/top");
