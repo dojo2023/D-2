50b38e249f4995d54924b9742d4fd4af32871b7e
dojo2023-fujita <130331187+dojo2023-fujita@users.noreply.github.com>
2023-06-27 10:26:41 +0900

    各種更新

--- src/src/servlet/PostServlet.java
+++ src/src/servlet/PostServlet.java
@@ -53,6 +53,21 @@ public class PostServlet extends HttpServlet {
 		String articleCareer = request.getParameter("career");
 		String[] articleCertification = request.getParameterValues("certification");
 		String articleText = request.getParameter("article_text");
+		//確認用にコンソールに表示
+		System.out.printf("articleTitle=%s\n",articleTitle);
+		System.out.printf("UserId=%s\n",userId);
+		System.out.printf("articleCareer=%s\n",articleCareer);
+		System.out.printf("articleText=%s\n",articleText);
+		for(int i=0; i<articleLanguage.length; i++) {
+			System.out.printf("articleLanguage=%s\n",articleLanguage[i]);
+		}
+		for(int i=0; i<articlePurpose.length; i++) {
+			System.out.printf("article_purpose=%s\n",articlePurpose[i]);
+		}
+		for(int i=0; i<articleCertification.length; i++) {
+			System.out.printf("article_Certification=%s\n",articleCertification[i]);
+		}
+
 
 		//リクエストスコープに格納
 		request.setAttribute("articleTitle",articleTitle);
